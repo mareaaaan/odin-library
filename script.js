7e65f155465376f659bb432a60394bcbc86bb7b1
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-06-30 16:37:45 +0100

    Add button to display form

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let books = [];
 const booksContainer = document.getElementsByClassName("books-container")[0];
+const addIcon = document.getElementsByClassName("icon--add")[0];
+const form = document.getElementsByClassName("form")[0];
+const body = document.getElementsByTagName("BODY")[0];
+const layer = document.getElementsByClassName("layer")[0];
 
 function Book(title, author, pageNumber, isRead) {
 	this.title = title;
@@ -41,7 +45,10 @@ function createBookCard(book) {
 }
 
 function displayBookCard(book) {
-	booksContainer.appendChild(createBookCard(book));
+	booksContainer.insertBefore(
+		createBookCard(book),
+		booksContainer.children[booksContainer.children.length - 1]
+	);
 }
 
 function displayBooks() {
@@ -54,9 +61,30 @@ addBookToLibrary("Ma cac printre stele", "Marian Gartu", 169, true);
 addBookToLibrary("Ce frumoasa este viata", "Vasilica Ceferistul", 10, false);
 addBookToLibrary("Ma-ta e o vaca", "Ciprian Ciohodaru", 341, true);
 addBookToLibrary("Nu am avut alternativa", "Catalin Gartu", 542, false);
-addBookToLibrary("Nu am avut alternativa", "Catalin Gartu", 542, false);
-addBookToLibrary("Nu am avut alternativa", "Catalin Gartu", 542, false);
-addBookToLibrary("Nu am avut alternativa", "Catalin Gartu", 542, false);
-addBookToLibrary("Nu am avut alternativa", "Catalin Gartu", 542, false);
+
+function blurBody() {
+	const layer = document.getElementsByClassName("layer")[0];
+	layer.classList.add("blur");
+}
+
+function unBlurBody() {
+	const layer = document.getElementsByClassName("layer")[0];
+	layer.classList.remove("blur");
+}
+
+function showAddBookForm() {
+	blurBody();
+	body.appendChild(form);
+}
+
+function hideAddBookForm() {
+	unBlurBody();
+	form.reset();
+	form.remove();
+}
+
+addIcon.addEventListener("click", showAddBookForm);
+layer.addEventListener("click", hideAddBookForm);
 
 displayBooks();
+hideAddBookForm();
